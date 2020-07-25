1c70eacc4fcce23e984528476ef678bf30e3b17c
Olanshile Jimoh <51200418+olanshilejay@users.noreply.github.com>
2020-07-25 11:47:17 +0100

    Create motion_led.ino

---
+++ motion_led.ino
@@ -0,0 +1,28 @@
+int ledLight = 7;
+int relaySwitch = 8;
+int PIR = 11;
+int button = 13;
+int value = LOW;
+
+void setup() {
+  pinMode(ledLight, OUTPUT);
+  pinMode (relaySwitch, OUTPUT);
+  pinMode(PIR, INPUT);
+  Serial.begin(9600); //set the board rate for displaying text on the serial monitor
+}
+
+void loop(){
+  value = digitalRead(PIR);
+  if (value == HIGH) {
+    digitalWrite(ledLight, HIGH);
+    digitalWrite (relaySwitch, HIGH);
+    Serial.println ("Movement Detected!!!");
+    delay(15000);
+  } 
+  else {
+      digitalWrite (ledLight, LOW);
+      digitalWrite (relaySwitch, LOW);
+      Serial.println("No Movement!!!");
+      delay(200);
+  }
+}
