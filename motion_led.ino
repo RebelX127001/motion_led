eca8a2ffc9c8e6b8001092e0c60498c5e6d37b33
Olanshile Jimoh <51200418+olanshilejay@users.noreply.github.com>
2020-09-05 17:49:02 +0100

    Create motion_led.ino

---
+++ motion_led.ino
@@ -0,0 +1,35 @@
+//declare variable to store pin value for led
+int led = 13;
+//declare variable to store pin value for pir sensor
+int sensor = 2;
+
+void setup() {
+  //make led which is pin 13 an output
+  pinMode(led, OUTPUT);
+  //make sensor which is in pin 2 an input
+  pinMode(sensor, INPUT);
+  //the transfer capacity for the serial port
+  Serial.begin(9600);
+}
+
+void loop() {
+  //declare variable sensorval and assign the sensor value to it
+  int sensorval = digitalRead(sensor);
+
+  //if sensor value is high
+  if (sensorval == HIGH) {
+    //display motion detected and turn on led
+    Serial.println("Motion Detected!");
+    digitalWrite(led, HIGH);
+    //pause the program for 1 second
+    delay(1000);
+  }
+  //else if sensor value is low
+  else {
+    //display motion stopped and turn the led off
+    Serial.println("No Motion!");
+    digitalWrite(led, LOW);
+    //pause the program for 1 second
+    delay(1000);
+  }
+}
