a3e0bf794dd6bc90db877d04b8810028fef70938
RebelX <51200418+RebelX127001@users.noreply.github.com>
2021-06-12 12:26:15 +0100

    Update motion_sec.ino

--- motion_sec.ino
+++ motion_sec.ino
@@ -7,7 +7,7 @@ int value = LOW;
 void setup() {
   pinMode(ledLight, OUTPUT);
   pinMode (relaySwitch, OUTPUT);
-  //declare pin 11 as O
+  //declare pin 11 as
   pinMode(PIR, INPUT);
   Serial.begin(9600);
 }
