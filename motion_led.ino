8a3305191b8273a798a3f6032e45db9a6a80e3f4
RebelX <51200418+RebelX127001@users.noreply.github.com>
2021-06-12 12:46:08 +0100

    Update motion_led.ino

--- motion_led.ino
+++ motion_led.ino
@@ -17,7 +17,7 @@ void loop() {
   else {
     Serial.println("No Motion!");
     digitalWrite(led, LOW);
-    //pause the program for 1 second
+    //pause the program for 1 sec
     delay(1000);
   }
 }
