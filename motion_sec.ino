18879e89c6727bfc4f42b026337fce666e34a975
RebelX <51200418+RebelX127001@users.noreply.github.com>
2021-06-12 12:19:28 +0100

    Update motion_sec.ino

--- motion_sec.ino
+++ motion_sec.ino
@@ -9,6 +9,7 @@ void setup() {
   pinMode(ledLight, OUTPUT);
   //declare pin 8 as Output
   pinMode (relaySwitch, OUTPUT);
+  //declare pin 11 as Output
   pinMode(PIR, INPUT);
   Serial.begin(9600); //set the board rate for displaying text on the serial monitor
 }
