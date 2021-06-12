da2ddee0e7b68bc36d9e9418c69a93cc0c60c365
RebelX <51200418+RebelX127001@users.noreply.github.com>
2021-06-12 12:37:09 +0100

    Update motion_led.ino

--- motion_led.ino
+++ motion_led.ino
@@ -8,7 +8,7 @@ void setup() {
 }
 
 void loop() {
-  //declare variable sensorval and the value it
+  //declare variable sensorval the value it
   int sensorval = digitalRead(sensor);
 
   //if sensor value is high
