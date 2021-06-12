4d14f6c373032519f635a42b036e958ae20d23ce
RebelX <51200418+RebelX127001@users.noreply.github.com>
2021-06-12 12:34:11 +0100

    Update motion_led.ino

--- motion_led.ino
+++ motion_led.ino
@@ -3,7 +3,7 @@ int sensor = 2;
 
 void setup() {
   pinMode(led, OUTPUT);
-  //make sensor which
+  //make sensor
   pinMode(sensor, INPUT);
   //the transfer capacity for the serial port
   Serial.begin(9600);
