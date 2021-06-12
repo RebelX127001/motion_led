0a80e95315d35547df631e9e6889326730b7aa34
RebelX <51200418+RebelX127001@users.noreply.github.com>
2021-06-12 12:30:56 +0100

    Update motion_led.ino

--- motion_led.ino
+++ motion_led.ino
@@ -2,7 +2,7 @@ int led = 13;
 int sensor = 2;
 
 void setup() {
-  //make led which is pin 13 an output
+  //make led which is pin 13 an
   pinMode(led, OUTPUT);
   //make sensor which is in pin 2 an input
   pinMode(sensor, INPUT);
