a49a314fcb3d9a58dac342af106a7a344a77e25b
RebelX <51200418+RebelX127001@users.noreply.github.com>
2021-06-12 12:43:21 +0100

    Update motion_led.ino

--- motion_led.ino
+++ motion_led.ino
@@ -14,7 +14,7 @@ void loop() {
     digitalWrite(led, HIGH);
     delay(1000);
   }
-  //else if sensor
+  //else if
   else {
     //display motion stopped and turn the led off
     Serial.println("No Motion!");
